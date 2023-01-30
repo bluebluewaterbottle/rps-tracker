13c9d785268bd11acf966635e0febabac9c314ea
bluebluewaterbottle <122560360+bluebluewaterbottle@users.noreply.github.com>
2023-01-30 13:26:51 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+function Update_Scoreboard () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player A:" + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player B:" + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds:" + Rounds)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties:" + Ties)
+}
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+let Ties = 0
+let Rounds = 0
+let PB = 0
+let PA = 0
+OLED.init(128, 64)
+OLED.writeStringNewLine("Want to play?")
+PA = 0
+PB = 0
+Rounds = 0
+Ties = 0
+basic.pause(2000)
+Update_Scoreboard()
