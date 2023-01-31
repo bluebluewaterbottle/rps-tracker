7f8c043c384936bbf3d92119f01113cd00065ada
bluebluewaterbottle <122560360+bluebluewaterbottle@users.noreply.github.com>
2023-01-31 13:26:09 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,17 +18,20 @@ input.onButtonPressed(Button.B, function () {
 	
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    Reset()
 })
+function Reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Want to play?")
+    PA = 0
+    PB = 0
+    Rounds = 0
+    Ties = 0
+    basic.pause(2000)
+    Update_Scoreboard()
+}
 let Ties = 0
 let Rounds = 0
 let PB = 0
 let PA = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Want to play?")
-PA = 0
-PB = 0
-Rounds = 0
-Ties = 0
-basic.pause(2000)
-Update_Scoreboard()
+Reset()
